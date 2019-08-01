3255e17c7438a9f3a4405fc7829fad31b850ade9
vylitalofs <vylitalofs@gmail.com>
2019-08-01 13:12:30 +0300

    Backend: Add mongoose/mongodb

--- app/backend/app.js
+++ app/backend/app.js
@@ -8,6 +8,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+// MongoDB with Mongoose
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost/cep';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
