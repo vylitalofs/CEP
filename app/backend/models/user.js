f719a235ceaf08e211a0c4208e9d562136f9605e
vylitalofs <vylitalofs@gmail.com>
2019-08-02 09:05:48 +0300

    Backend: User Model: Add isDisabled

--- app/backend/models/user.js
+++ app/backend/models/user.js
@@ -6,16 +6,15 @@ var UserSchema = new Schema(
     name: {type: String, required: true, min: 1, max: 100},
     email: {type: String, required: true, min: 3, max: 100},
     password: {type: String, required: true, min: 4, max: 32},
-    isAdmin: {type: Boolean},
+    isAdmin: {type: Boolean, required: true},
+    isDisabled: {type: Boolean, required: true}
   }
 );
 
-// Virtual for user's URL
 UserSchema
 .virtual('url')
 .get(function () {
   return '/api/user/' + this._id;
 });
 
-//Export model
 module.exports = mongoose.model('User', UserSchema);
