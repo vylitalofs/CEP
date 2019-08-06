8c992d2cc83f3faa7069f7bd896f77bc444fc936
vylitalofs <vylitalofs@gmail.com>
2019-08-06 14:03:34 +0300

    Backend: Usercontroller: Added, detail, update, remove

--- app/backend/controllers/userController.js
+++ app/backend/controllers/userController.js
@@ -2,29 +2,61 @@ var User = require('../models/user');
 const { body, validationResult } = require('express-validator');
 const { sanitizeBody } = require('express-validator');
 
-// Display list of all Users on users GET
+
+// GET User
+exports.user_detail = function(req, res, next) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) { return next(err); }
+        if (user == null) {
+            var err = new Error('User not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.status(200).json(user);
+    });
+}
+
+// GET User List
 exports.user_list = function(req, res, next) {
   User.find()
-    .sort([['family_name', 'ascending']])
+    .sort([['lastName', 'ascending']])
     .exec(function (err, list_users) {
       if (err) { return next(err); }
       res.json(list_users)
     });
 };
 
-// Handle User create on POST.
-exports.user_create_post = [
+// DELETE User
+exports.user_delete = function(req, res, next) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) { return next(err); }
+        if (user == null) {
+            var err = new Error('User not found');
+            err.status = 404;
+            return next(err);
+        }
+        user.remove();
+        res.status(200).json({"message":"success"});
+    });
+};
+
+// POST, User Create 
+exports.user_create = [
 
     // Validate fields.
-    body('name').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+    body('firstName').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
         .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+
+    body('lastName').isLength({ min: 1 }).trim().withMessage('Last name must be specified.')
+        .isAlphanumeric().withMessage('Last name has non-alphanumeric characters.'),
 
     body('email').isLength({ min: 3 }).trim().isEmail().normalizeEmail().withMessage('Email-address must be specified.'),
 
     body('password').isLength({ min: 3 }).trim().withMessage('Password must be specified.'),
 
     // Sanitize fields.
-    sanitizeBody('name').escape(),
+    sanitizeBody('firstName').escape(),
+    sanitizeBody('lastName').escape(),
     sanitizeBody('email').escape(),
     sanitizeBody('password').escape(),
     sanitizeBody('isAdmin').toBoolean(),
@@ -37,15 +69,14 @@ exports.user_create_post = [
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            // TODO: Handle errors
             res.status(500).json(errors);
             return;
         }
         else {
-            // Form data is valid, create user
             var user = new User(
                 {
-                    name: req.body.name,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
                     email: req.body.email,
                     password: req.body.password,
                     isAdmin: req.body.isAdmin,
@@ -55,6 +86,67 @@ exports.user_create_post = [
             user.save(function (err) {
                 if (err) { return next(err); }
                 res.status(200).json({"message":"success"});
+            });
+        }
+    }
+];
+
+// PUT, User Update 
+exports.user_update = [
+
+     // Validate fields.
+    body('firstName').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+
+    body('lastName').isLength({ min: 1 }).trim().withMessage('Last name must be specified.')
+        .isAlphanumeric().withMessage('Last name has non-alphanumeric characters.'),
+
+    body('email').isLength({ min: 3 }).trim().isEmail().normalizeEmail().withMessage('Email-address must be specified.'),
+
+    body('password').isLength({ min: 3 }).trim().withMessage('Password must be specified.'),
+
+    // Sanitize fields.
+    sanitizeBody('firstName').escape(),
+    sanitizeBody('lastName').escape(),
+    sanitizeBody('email').escape(),
+    sanitizeBody('password').escape(),
+    sanitizeBody('isAdmin').toBoolean(),
+    sanitizeBody('isDisabled').toBoolean(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.status(500).json(errors);
+            return;
+        }
+        else {
+            User.findById(req.params.id, function (err, user) {
+                if (err) { return next(err); }
+                if (user == null) {
+                    var err = new Error('User not found');
+                    err.status = 404;
+                    return next(err);
+                }
+                
+                var newuser = new User(
+                {
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    password: req.body.password,
+                    isAdmin: req.body.isAdmin,
+                    isDisabled: req.body.isDisabled,
+                    _id: req.params.id
+                });
+
+                user.update(newuser, function (err) {
+                    if (err) { return next(err); }
+                    res.status(200).json({"message":"success"});
+                });
             });
         }
     }
--- app/backend/routes/api.js
+++ app/backend/routes/api.js
@@ -8,7 +8,16 @@ var userController = require('../controllers/userController');
 router.get('/api/users', userController.user_list);
 
 // POST request for creating User.
-router.post('/api/user/create', userController.user_create_post);
+router.post('/api/user/create', userController.user_create);
+
+// GET request for one User.
+router.get('/api/user/:id', userController.user_detail);
+
+// PUT request for updating User.
+router.put('/api/user/:id/', userController.user_update);
+
+// DELETE request for removing User.
+router.delete('/api/user/:id/', userController.user_delete);
 
 
 
