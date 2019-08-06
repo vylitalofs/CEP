647227f493330f08de384c6be0c5be599c0d2758
vylitalofs <vylitalofs@gmail.com>
2019-08-06 14:03:00 +0300

    Backend: UserModel: Split name into first and last

--- app/backend/models/user.js
+++ app/backend/models/user.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema(
   {
-    name: {type: String, required: true, min: 1, max: 100},
+    firstName: {type: String, required: true, min: 1, max: 100},
+    lastName: {type: String, required: true, min: 1, max: 100},
     email: {type: String, required: true, min: 3, max: 100},
     password: {type: String, required: true, min: 4, max: 32},
     isAdmin: {type: Boolean, required: true},
@@ -15,6 +16,12 @@ UserSchema
 .virtual('url')
 .get(function () {
   return '/api/user/' + this._id;
+});
+
+UserSchema
+.virtual('name')
+.get(function () {
+  return this.lastName + ', ' + this.firstName;
 });
 
 module.exports = mongoose.model('User', UserSchema);
