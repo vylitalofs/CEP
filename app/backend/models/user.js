5a97bd223875e51ffd3c03cea781aed53d471c4c
vylitalofs <vylitalofs@gmail.com>
2019-08-08 14:09:13 +0300

    Backend: Implemented Auth, Sessions

--- app/backend/models/user.js
+++ app/backend/models/user.js
@@ -1,16 +1,28 @@
 var mongoose = require('mongoose');
+const crypto = require('crypto');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema(
   {
     firstName: {type: String, required: true, min: 1, max: 100},
     lastName: {type: String, required: true, min: 1, max: 100},
-    email: {type: String, required: true, min: 3, max: 100},
-    password: {type: String, required: true, min: 4, max: 32},
+    email: {type: String, required: true, unique:true, min: 3, max: 100},
     isAdmin: {type: Boolean, required: true},
-    isDisabled: {type: Boolean, required: true}
+    isDisabled: {type: Boolean, required: true},
+    hash: String,
+    salt: String,
   }
 );
+
+UserSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validatePassword = function(password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
 
 UserSchema
 .virtual('url')
--- app/backend/routes/login.js
+++ app/backend/routes/login.js
@@ -1,5 +1,85 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+const router = require('express').Router();
+const User = mongoose.model('User');
+const Session = require("../models/session");
 
+let ttl_diff = 1000*60*60;
+
+generateToken = function() {
+  let token = ""
+  let letters = '^_!#$%&*+-0123456789<=>?@ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz'
+  for(let i=0;i<1024;i++) {
+    let temp = Math.floor(Math.random()*75);
+    token = token + letters[temp]
+  }
+  return token;
+}
+
+router.post("/logout", function(req, res) {
+	let token = req.headers.token;
+	if(token) {
+		Session.findOne({"token":token}, function(err, session) {
+			if(err) {
+				console.log("Find session failed. Still logging out");
+			}
+			session.remove()
+		});
+	}
+	return res.status(200).json({"message":"success"});
+})
+
+router.post("/login", function(req, res) {
+
+	if(!req.body || !req.body.email || !req.body.password) {
+		return res.status(422).json({"error":"wrong credentials"});		
+	}
+
+	if(req.body.email.length < 4 || req.body.password.length < 8) {
+		return res.status(422).json({"error":"wrong credentials"});		
+	}
+
+	User.findOne({"email":req.body.email}, function(err, user) {
+
+		if(err || !user) {
+			return res.status(422).json({"message":"wrong credentials"});			
+		}
+
+		if (user.validatePassword(req.body.password)) {
+
+			let token = generateToken();
+			let ttl = new Date().getTime()+ttl_diff;
+			let session = new Session({
+				"username":user.username,
+				"ttl":ttl,
+				"token":token
+			})
+
+			session.save(function(err, session) {
+				if(err) {
+					return res.status(422).json({"error":"wrong credentials"});						
+				} 
+				return res.status(200).json({"token":token});
+			});
+
+		} else {
+			return res.status(422).json({"error":"wrong credentials"});				
+		}
+		
+	});
+});
+
+//GET current route (required, only authenticated users have access)
+router.get('/current', (req, res, next) => {
+  const { payload: { id } } = req;
+
+  return User.findById(id)
+    .then((user) => {
+      if(!user) {
+        return res.sendStatus(400);
+      }
+
+      return res.json({ user: user.toAuthJSON() });
+    });
+});
 
 module.exports = router;
