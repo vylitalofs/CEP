0ab7d88a76e1ffe3f9d223d5f0ae939ae1420fca
vylitalofs <vylitalofs@gmail.com>
2019-08-09 15:22:15 +0300

    Frontend: Fix warning in NormalRow.js

--- app/frontend/src/NormalRow.js
+++ app/frontend/src/NormalRow.js
@@ -7,7 +7,6 @@ export default class NormalRow extends React.Component {
 	render() {
 		return (
 			<Table.Row>
-         //create a list of usernames, email and access rights
 				<Table.Cell>{this.props.item.username}</Table.Cell>
 				<Table.Cell>{this.props.item.email}</Table.Cell>
 				<Table.Cell>{this.props.item.isAdmin}</Table.Cell>
