6a9cb91e8ebf862b11212dc7d57fc215aca909cb
Joona <jookinnu@gmail.com>
2019-08-17 17:12:48 +0300

    Revert "Frontend:App updates"
    This reverts commit 3ebd11ed1468decba3f8d8357852afd54a7fd9fb.

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -131,16 +131,7 @@ class App extends React.Component {
 
                             <Route path="/user/:id" render={({match}) =>
                                 this.state.isLogged ?
-                                    <UserView token={this.state.token} id={match.params.id}
-                                    onSubmitRemove={this.onSubmitRemove}
-                                    editUser={this.editUser}/> :
-                                    <Redirect to="/"/>
-                            }/>
-                            <Route path="/case/:id" render={({match}) =>
-                                this.state.isLogged ?
-                                    <CaseView token={this.state.token} id={match.params.id}
-                                    onSubmitRemove={this.onSubmitRemove}
-                                    editCase={this.editCase}/> :
+                                    <UserView token={this.state.token} id={match.params.id}/> :
                                     <Redirect to="/"/>
                             }/>
 
